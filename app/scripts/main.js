9596f578d0c85e6f92eeb507cadce96bc8f2f35d
Topher Young <topheryoung@LW-CHS-MAC02.local>
2015-07-10 15:57:12 -0400

    Bug fixing and including use strict statements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,17 @@
 /*
- * Title: Javascript for L.E.S.
- * Author: Topher Young
- * Version: 1.0
+ * Filename: main.js
  *
-*/
+ * Author(s): Topher Young
+ *
+ * Scripts for the cty_slider_nav 
+ * 
+ */
 
-
+/*
+ * Create function to remove/add class to navigation bar
+ */
 function toggleNav() {
+	'use strict';
     if ($('.ctyNavbar').hasClass('show-nav')) {
         $('.ctyNavbar').removeClass('show-nav').addClass('hide-nav');
     } else 
@@ -18,17 +23,25 @@ function toggleNav() {
     }
 }
 
+/*
+ * Trigger and call the toggleNav() function
+ */
 $(function() {
+	'use strict';
     $('.toggle-nav').click(function() {
         toggleNav();
     });
 });
 
+/*
+ * Animate hamburger icon when clicked
+ */
 $(function() {
-$('.nav-trigger').click(function(e) {
-  e.preventDefault();
-  $('.trigger-top, .trigger-middle, .trigger-bottom').toggleClass('open');
-});
+	'use strict';
+	$('.nav-trigger').click(function(e) {
+	  e.preventDefault();
+	  $('.trigger-top, .trigger-middle, .trigger-bottom').toggleClass('open');
+	});
 });
 
 
